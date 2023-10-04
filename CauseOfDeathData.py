cae30bad2361df7dc955ab32bd5145064ef145a5
Patrick Coady <pcoady@bamboohealth.com>
2023-10-04 15:30:30 -0500

    Updated to use SQL quires

--- CauseOfDeathData.py
+++ CauseOfDeathData.py
@@ -17,17 +17,39 @@ from LifeData import LifeData
 import Partisian as Partisian
 from sklearn.linear_model import LinearRegression
 import addfips
+import psycopg2
 
+import warnings
+
+
+
+
+
+def get_db_properties():
+    props = {}
+    separator = "="
+            
+    with open(r"C:\Users\zeusg\OneDrive\Documents\Github\habit_final\database.properties") as file:
+        for line in file: 
+            if separator in line:
+                name, value = line.split(separator, 1)
+                props[name.strip()] = value.strip()
+    return props
+
+def get_db_connection():
+    props = get_db_properties()
+    connection = psycopg2.connect(
+        host=props["host"],
+        user=props["username"],
+        password=props["password"],
+        database="StateOfStates")
+    return connection
 
 def translate_ICD10(x, conversion):
     try:
         return conversion[x]
     except:
         return x
-    
-    
-
-
 
 def clean_state_data(df):
     af = addfips.AddFIPS()
@@ -155,15 +177,74 @@ def get_age_cod(df, age, all_ages=False, state="Michigan"):
             values[0] += row["Child_Adj_Rate"]
         values.append(row["Child_Adj_Rate"])
     return labels, parents, values, df, values[0]
+
+
+
+def get_age_cod_db(age, all_ages=False, state="Michigan"):
+    '''Returns a list of labels, parents, and values for a given state and age group'''
+    
+    # TODO: move this to YAML
+    sql = "select l.name as state, l.acronym, l.fips, \
+            cod.rate, cod.deaths, cod.population, cod.start_age, cod.end_age, \
+            ic.name, ic.common_name, \
+            ic2.name as parent_name, ic2.common_name as parent_common_name \
+            from causes_of_death cod \
+            inner join icd_10_codes ic on cod.icd_10_codes_id_fkey = ic.id \
+            left join icd_10_codes ic2 on ic.parent = ic2.id \
+            inner join locations l ON cod.locations_id_fkey = l.id \
+            where l.level = 'State'"
+            
+    con = get_db_connection()
+    
+    df = pd.read_sql(sql, con)
+    df = df[df["start_age"] ==age]
+    df = df[df["state"] == state]
+    df = adjust_parent_rate(df)
+    
+    
+    labels = [age]
+    parents = [""]
+    if all_ages:
+        parents = [state]
+    values = [0]
+    values.extend(list(df["rate"]))
+    for i, row in df.iterrows():
+        labels.append(row["common_name"]+ ": " + str(row["start_age"]) + "-" + str(row["end_age"]))
+        if row["parent_common_name"]!= None:
+            parents.append(row["parent_common_name"]+ ": " + str(row["start_age"]) + "-" + str(row["end_age"]))
+        else: 
+            parents.append(age)
+            values[0] += row["rate"]
+    return labels, parents, values, df, values[0]
+
+def adjust_parent_rate(df):
+    for i in range(5):
+        for name in df["parent_name"].unique():
+            age = df["start_age"].unique()[0]
+            if name == "Other heart diseases (I26-I51)" and age==55:
+                print("HERE")
+            parent_df = df[df["parent_name"] == name]
+            parent_rate = parent_df["rate"].sum()
+            orignal_rate = 0
+            try:
+                orignal_rate = df[df["name"] == name]["rate"].values[0]
+            except:
+                pass
+            if parent_rate > orignal_rate:
+                df.loc[df["name"] == name, "rate"] = parent_rate
+    return df
+    
+
     
 def get_all_age_cod(state = "Michigan"):
     '''Returns a list of labels, parents, and values for a given state and all age groups'''
-    df = load_state_cod(state = state)
+    #df = load_state_cod(state = state)
+    ages = [0, 1, 5, 15, 25, 35, 45, 55, 65, 75, 85]
     all_labels = [state]
     all_parents = [""]
     all_values = [0]
-    for age in df["Ten-Year Age Groups Code"].unique():
-        labels, parents, values, df_age, total = get_age_cod(df, age, True, state)
+    for age in ages:
+        labels, parents, values, df_age, total = get_age_cod_db(age, True, state)
         all_labels.extend(labels)
         all_parents.extend(parents)
         all_values.extend(values)
